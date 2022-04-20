ff0f06dbbfaaf9f89204621d5a1b1f8c1f9e38ec
[4기-1반] 김경빈 <bin9610@gmail.com>
2022-04-20 17:27:17 +0900

    Add : add userMVP

---
+++ back/src/db/schemas/user.js
@@ -0,0 +1,38 @@
+const { Schema, model } = require("mongoose")
+
+const UserSchema = new Schema(
+    {
+        id: {
+            type: String,
+            required: true,
+        },
+        email: {
+            type: String,
+            required: true,
+        },
+        name: {
+            type: String,
+            required: true,
+        },
+        nickname: {
+            type: String,
+            required: true,
+        },
+        password: {
+            type: String,
+            required: false
+        },
+        description: {
+            type: String,
+            required: false,
+            default: "설명이 아직 없습니다. 추가해 주세요.",
+        },
+    },
+    {
+        timestamps: true
+    }
+)
+
+const UserModel = model("User", UserSchema)
+
+module.exports = { UserModel }
