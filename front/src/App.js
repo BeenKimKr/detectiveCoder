00098dbac154bf2621e98f0e82f3e7609dc43d8a
Hailey_ys <whassup_man@naver.com>
2022-04-20 22:35:30 +0900

    Setting tailwind

--- front/src/App.js
+++ front/src/App.js
@@ -1,7 +1,7 @@
-import './App.css';
+import "./App.css";
 
 function App() {
-  return <div className='App'></div>;
+  return <h1 className="App text-3xl font-bold underline ">안녕</h1>;
 }
 
 export default App;
