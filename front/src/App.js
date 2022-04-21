2c5cc76519c6abe933e1ffc9166640aa9020bdb9
Jeongsiwook <jsu0638@naver.com>
2022-04-21 12:04:32 +0900

    Update front

--- front/src/App.js
+++ front/src/App.js
@@ -1,24 +1,20 @@
-import React, { useState, useEffect, useReducer, createContext } from 'react'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import { useNavigate } from 'react-router-dom'
-
-import Home from './Home'
-import Portfolio from './Portfolio'
+import React, { useState, useEffect, useReducer, createContext } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './Home';
+import Portfolio from './Portfolio';
 
 function App() {
-  const navigate = useNavigate()
-
   return (
     <>
       <Router>
         <Routes>
-          <Route path="/" exact element={<Home />} />
-          <Route path="/mypage" exact element={<Portfolio />} />
-          <Route path="*" element={<Home />} />
+          <Route path='/' exact element={<Home />} />
+          <Route path='/mypage' exact element={<Portfolio />} />
+          <Route path='*' element={<Home />} />
         </Routes>
       </Router>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
