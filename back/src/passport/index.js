6b7dd21b727521f36696a717e744b79e24f894d3
[4기-1반] 김경빈 <bin9610@gmail.com>
2022-04-22 17:24:14 +0900

    Add kakao module incompletion

--- back/src/passport/index.js
+++ back/src/passport/index.js
@@ -1,9 +1,11 @@
 const passport = require("passport");
 const { User } = require("../db");
+const { kakao } = require("./strategy/kakao");
 const { naver } = require("./strategy/naver");
 
 module.exports = () => {
 
 
     naver();
+    kakao();
 };
---
+++ back/src/passport/strategy/kakao.js
@@ -0,0 +1,9 @@
+const passport = require("passport");
+const { User } = require('../../db');
+const KakaoStrategy = require("passport-kakao").Strategy;
+
+const kakao = () => {
+
+};
+
+module.exports = { kakao };
