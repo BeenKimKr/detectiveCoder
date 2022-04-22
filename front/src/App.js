f634d420001dddbfcc31eda8a273bc2d4cad0d11
Naayeon <naaay097@gmail.com>
2022-04-23 01:51:40 +0900

    Remove Home.js

--- front/src/App.js
+++ front/src/App.js
@@ -1,16 +1,14 @@
 import React, { useState, useEffect, useReducer, createContext } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './Home';
-import Portfolio from './Portfolio';
+import Home from './pages/Home';
 
 function App() {
   return (
     <>
       <Router>
         <Routes>
-          <Route path='/' exact element={<Home />} />
-          <Route path='/mypage' exact element={<Portfolio />} />
-          <Route path='*' element={<Home />} />
+          <Route path="/" exact element={<Home />} />
+          <Route path="*" element={<Home />} />
         </Routes>
       </Router>
     </>
