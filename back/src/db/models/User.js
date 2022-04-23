b518c395ca739bd060a11c29aa491256d6ef5df0
[4기-1반] 김경빈 <bin9610@gmail.com>
2022-04-24 00:05:51 +0900

    Fix userMVP model

--- back/src/db/models/User.js
+++ back/src/db/models/User.js
@@ -1,18 +1,13 @@
 const { UserModel } = require("../schemas/user");
 
 const User = {
-    create: async ({ newUser }) => {
+    create: async (newUser) => {
         const createdNewUser = await UserModel.create(newUser);
         return createdNewUser;
     },
 
     findById: async ({ id }) => {
-        const user = await UserModel.findOne(id);
-        return user;
-    },
-
-    findByEmail: async ({ email }) => {
-        const user = await UserModel.findOne(email);
+        const user = await UserModel.findOne({ id });
         return user;
     },
 
