5efd03dff61b2fb3210688f1cbe5d7be0c4ab133
[4기-1반] 양권민 <didrnjsals@gmail.com>
2022-04-24 00:35:53 +0900

    Fix DB_URL

--- back/src/db/index.js
+++ back/src/db/index.js
@@ -1,17 +1,16 @@
 const mongoose = require("mongoose");
 const { User } = require("./models/User");
 
-const DB_URL =
-    process.env.MONGODB_URL || "mongo_url";
+const DB_URL = process.env.MONGODB_URL || "mongo_url";
 
 mongoose.connect(DB_URL);
 const db = mongoose.connection;
 
 db.on("connected", () =>
-    console.log("정상적으로 MongoDB 서버에 연결되었습니다.  " + DB_URL)
+  console.log("정상적으로 MongoDB 서버에 연결되었습니다.  " + DB_URL)
 );
 db.on("error", (error) =>
-    console.error("MongoDB 연결에 실패하였습니다...\n" + DB_URL + "\n" + error)
+  console.error("MongoDB 연결에 실패하였습니다...\n" + DB_URL + "\n" + error)
 );
 
 module.exports = { User };
