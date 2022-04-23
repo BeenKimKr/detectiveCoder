8e05efa8cf0b95b18af5f439e05ecbc0893c4226
[4기-1반] 양권민 <didrnjsals@gmail.com>
2022-04-24 00:38:53 +0900

    Fix userAuthService

--- back/src/services/userService.js
+++ back/src/services/userService.js
@@ -1,6 +1,13 @@
 const { User } = require("../db");
 const { v4: uuidv4 } = require("uuid");
 
-const userAuthService = {};
+const userAuthService = {
+  addUser: async ({ name, email }) => {
+    const id = uuidv4();
+    const newUser = { id, email, name };
+    const createdNewUser = await User.create({ newUser });
+    return createdNewUser;
+  },
+};
 
 module.exports = { userAuthService };
