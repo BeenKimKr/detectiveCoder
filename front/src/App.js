a45afe28d704271b97ded077d42615dc10993734
Naayeon <naaay097@gmail.com>
2022-04-24 03:09:52 +0900

    Fix App.js

--- front/src/App.js
+++ front/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
+
 import CityInfo from './pages/cityInfo/CityInfo';
 import './App.css';
 
@@ -9,9 +9,9 @@ function App() {
     <>
       <Router>
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/cityinfo" exact element={<CityInfo />} />
-          <Route path="*" element={<Home />} />
+          <Route path="/cityinfo" element={<CityInfo />} />
+
+          <Route path="*" element={<CityInfo />} />
         </Routes>
       </Router>
     </>
