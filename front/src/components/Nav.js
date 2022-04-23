961a3d4a306cf42d332374d96c9450e5ca92a5c6
Hailey_ys <whassup_man@naver.com>
2022-04-24 01:09:14 +0900

    Fix mobile navbar

--- front/src/components/Nav.js
+++ front/src/components/Nav.js
@@ -14,7 +14,7 @@ const Navbar = () => {
         <button
           data-collapse-toggle='mobile-menu'
           type='button'
-          class='inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg sm:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600'
+          class='inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600'
           aria-controls='mobile-menu'
           aria-expanded='false'
         >
