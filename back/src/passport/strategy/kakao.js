1df82ea6640e0643119c1d601ac58b53abe0cdc7
[4기-1반] 김경빈 <bin9610@gmail.com>
2022-04-23 10:39:32 +0900

    Add kakao strategy for SNSlogin

--- back/src/passport/strategy/kakao.js
+++ back/src/passport/strategy/kakao.js
@@ -3,7 +3,36 @@ const { User } = require('../../db');
 const KakaoStrategy = require("passport-kakao").Strategy;
 
 const kakao = () => {
+    passport.use(
+        new KakaoStrategy(
+            {
+                clientId: process.env.KAKAO_SECRET_KEY,
+                callbackURL: '/users/kakao/callback'
+            },
+            async (accessToken, refreshToken, profile, done) => {
+                try {
+                    const user = await User.findOne({
+                        where: { id: profile.id, procider: 'kakao' }
+                    });
 
+                    if (user) {
+                        done(null, user);
+                    } else {
+                        const newUser = await User.create({
+                            email: profile._json && profile._json.kakao_account_email,
+                            name: profile.displayName,
+                            birthday: profile.birthday,
+                            provider: 'kakao',
+                        });
+                        done(null, newUser);
+                    }
+                } catch (error) {
+                    console.error(error);
+                    done(error);
+                }
+            }
+        )
+    );
 };
 
 module.exports = { kakao };
