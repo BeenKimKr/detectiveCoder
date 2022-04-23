40003e659623d00686fc50bf07da884792ce5443
[4기-1반] 김경빈 <bin9610@gmail.com>
2022-04-23 14:02:38 +0900

    Fix kakao strategy

--- back/src/passport/strategy/kakao.js
+++ back/src/passport/strategy/kakao.js
@@ -1,24 +1,25 @@
-const passport = require("passport");
 const { User } = require('../../db');
+const passport = require("passport");
 const KakaoStrategy = require("passport-kakao").Strategy;
 
 const kakao = () => {
     passport.use(
         new KakaoStrategy(
             {
-                clientId: process.env.KAKAO_SECRET_KEY,
+                clientID: process.env.KAKAO_SECRET_KEY,
                 callbackURL: '/users/kakao/callback'
             },
             async (accessToken, refreshToken, profile, done) => {
                 try {
                     const user = await User.findOne({
-                        where: { id: profile.id, procider: 'kakao' }
+                        where: { id: profile.id, provider: 'kakao' }
                     });
 
                     if (user) {
                         done(null, user);
                     } else {
                         const newUser = await User.create({
+                            id: profile.id,
                             email: profile._json && profile._json.kakao_account_email,
                             name: profile.displayName,
                             birthday: profile.birthday,
