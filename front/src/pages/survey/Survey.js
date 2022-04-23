86864f1009c322dac2cf0a13e4c171557496acbf
Naayeon <naaay097@gmail.com>
2022-04-23 22:30:20 +0900

    Commit

---
+++ front/src/pages/survey/Survey.js
@@ -0,0 +1,28 @@
+// 설문조사 페이지
+import React, { useState, createContext } from 'react';
+import WeatherSurvey from './WeatherSurvey';
+import ContinentsSurvey from './ContinentsSurvey';
+
+export const SaveAnswersContext = createContext();
+export const PagesContext = createContext();
+
+const Survey = () => {
+  const [answers, setAnswers] = useState([]);
+  const [pages, setPages] = useState(0);
+  const saveAnswers = { answers, setAnswers };
+  const changePage = { setPages };
+
+  console.log(pages);
+
+  return (
+    <div class="max-h-full">
+      <SaveAnswersContext.Provider value={saveAnswers}>
+        <PagesContext.Provider value={changePage}>
+          {pages === 0 ? <WeatherSurvey /> : <ContinentsSurvey />}
+        </PagesContext.Provider>
+      </SaveAnswersContext.Provider>
+    </div>
+  );
+};
+
+export default Survey;
