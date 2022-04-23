29ff43306275077c1d8a9f080d1b43f193254e4e
Jeongsiwook <jsu0638@naver.com>
2022-04-23 22:05:30 +0900

    Fix CityInfo.js

--- front/src/pages/CityInfo.js
+++ front/src/pages/CityInfo.js
@@ -1,7 +1,8 @@
 import React, { useState } from 'react';
 // import Nav from '../components/Nav';
 import WealChart from '../components/charts/WealChart';
-import Button from '@material-tailwind/react/Button';
+import Button from '../components/Button';
+import Image from '@material-tailwind/react/Image';
 
 const CityInfo = () => {
   const [name, setName] = useState('김나연');
@@ -9,27 +10,35 @@ const CityInfo = () => {
   const [city, setCity] = useState('베를린');
 
   return (
-    <>
+    <div className='container w-screen flex-col h-screen'>
       {/* <Nav /> */}
-      <div>
+      <div className='space-y-4'>
         {name}님께 {country}-{city}을(를) 추천합니다.
       </div>
-      <div>
-        <img src='../../public/GermanyFlag.png' alt='국기 사진' />
-        <div>
-          `우리 {country}은(는)요 150여개 국가 중 행복순위는 30번째구요.`
+      <div className='space-y-4 flex flex-row '>
+        <Image
+          className='basis-1/2'
+          src='https://upload.wikimedia.org/wikipedia/commons/thumb/b/ba/Flag_of_Germany.svg/383px-Flag_of_Germany.svg.png'
+          rounded={true}
+          raised={false}
+          alt='국기 사진'
+        />
+
+        <div className='basis-1/2'>
+          우리 {country}은(는)요 150여개 국가 중 행복순위는 30번째구요.
         </div>
       </div>
-      <div>{/* 나연님 차트 */}</div>
-      <div>
+      <div className='space-y-4'>{/* 나연님 차트 */}</div>
+      <div className='flex flex-row'>
+        <WealChart className='basis-1/2' />
         {/* 예솔님 차트 */}
-        <WealChart />
+        <WealChart className='basis-1/2' />
       </div>
-      <div>
-        <button>비슷한 도시 보기</button>
-        <button>저장하기</button>
+      <div className='flex space-x-4 justify-end'>
+        <Button text='비슷한 나라 보기' type='serve' />
+        <Button text='저장하기' type='main' />
       </div>
-    </>
+    </div>
   );
 };
 
