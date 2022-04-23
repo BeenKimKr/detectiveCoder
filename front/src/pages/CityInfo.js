fb6d001110b52eeaa51e47726db0b77427b3731f
Naayeon <naaay097@gmail.com>
2022-04-23 15:42:14 +0900

    Add HPIChart

--- front/src/pages/CityInfo.js
+++ front/src/pages/CityInfo.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import WeatherChart from '../components/charts/WeatherChart';
+import HPIChart from '../components/charts/HPIChart';
+
 const CityInfo = () => {
   return (
-    <div style={{ width: '500px', height: '300px' }}>
-      <WeatherChart />
-    </div>
+    <>
+      <div style={{ width: '500px', height: '300px' }}>
+        <WeatherChart />
+      </div>
+      <div style={{ width: '500px', height: '300px' }}>
+        <HPIChart />
+      </div>
+    </>
   );
 };
 
