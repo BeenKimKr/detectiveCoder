97b63c6990f2a8ddc1de14e9662a48dce541606f
[4기-1반] 양권민 <didrnjsals@gmail.com>
2022-04-24 00:38:39 +0900

    Fix code style

--- back/src/routers/userRouter.js
+++ back/src/routers/userRouter.js
@@ -1,17 +1,28 @@
 const { Router } = require("express");
 const passport = require("passport");
+const { userAuthService } = require("../services/userService");
 
 const userAuthRouter = Router();
 
-userAuthRouter.get(
-  "/naver",
-  passport.authenticate("naver", { authType: "reprompt" })
-);
+userAuthRouter.get("/naver", passport.authenticate("naver"));
 
 userAuthRouter.get(
   "/naver/callback",
-  //? 그리고 passport 로그인 전략에 의해 naverStrategy로 가서 카카오계정 정보와 DB를 비교해서 회원가입시키거나 로그인 처리하게 한다.
-  passport.authenticate("naver", { failureRedirect: "/" }),
+  passport.authenticate("naver", {
+    failureRedirect: "/",
+  }),
+  (req, res) => {
+    res.redirect("/");
+  }
+);
+
+userAuthRouter.get("/kakao", passport.authenticate("kakao"));
+
+userAuthRouter.get(
+  "/kakao/callback",
+  passport.authenticate("kakao", {
+    failureRedirect: "/",
+  }),
   (req, res) => {
     res.redirect("/");
   }
