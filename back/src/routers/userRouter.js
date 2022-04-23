b0b2d9387d39c5ab838e44593d5cc59e132e6bf9
[4기-1반] 김경빈 <bin9610@gmail.com>
2022-04-23 10:47:13 +0900

    Add kakao router for SNSlogin

--- back/src/routers/userRouter.js
+++ back/src/routers/userRouter.js
@@ -1,17 +1,29 @@
-const { Router } = require("express");
-const passport = require("passport");
-const { userAuthService } = require("../services/userService");
+const { Router } = require('express');
+const passport = require('passport');
+const { userAuthService } = require('../services/userService');
 
 const userAuthRouter = Router();
 
-userAuthRouter.get('/naver', passport.authenticate("naver", null), (req, res) => {
-    console.log("main");
+userAuthRouter.get('/naver', passport.authenticate('naver', null), (req, res) => {
+    console.log('main');
 });
 
-userAuthRouter.get('/naver/callback', passport.authenticate("naver", {
-    failureRedirect: "/"
+userAuthRouter.get('/naver/callback', passport.authenticate('naver', {
+    failureRedirect: '/'
 }), (req, res) => {
     res.status(200).send(req.user);
 });
+
+userAuthRouter.get('/kakao', passport.authenticate('kakao'));
+
+userAuthRouter.get(
+    '/kakao/callback',
+    passport.authenticate('kakao', {
+        failureRedirect: '/'
+    }),
+    (req, res) => {
+        res.status(200).send(req.user);
+    }
+);
 
 module.exports = { userAuthRouter };
