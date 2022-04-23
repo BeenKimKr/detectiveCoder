522a0eea0976962fec09e66588dfe461e4418f60
[4기-1반] 양권민 <didrnjsals@gmail.com>
2022-04-24 00:35:26 +0900

    Add passportConfig

--- back/src/app.js
+++ back/src/app.js
@@ -1,14 +1,10 @@
 const express = require("express");
+const passport = require("passport");
+const passportConfig = require("./passport");
 const { userAuthRouter } = require("./routers/userRouter");
 const { errorMiddleware } = require("./middlewares/errorMiddleware");
 
-const passport = require("passport");
-const passportConfig = require("./passport");
-
 const app = express();
-passportConfig();
-
-app.use(passport.initialize());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -22,6 +18,8 @@ app.get("/", (req, res) => {
   res.send("main page");
 });
 
+passportConfig();
+app.use(passport.initialize());
 app.use("/users", userAuthRouter);
 app.use(errorMiddleware);
 
