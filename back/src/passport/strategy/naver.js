aa24f4508305a965bac81fea0e48d33c0c960410
[4기-1반] 양권민 <didrnjsals@gmail.com>
2022-04-24 00:37:49 +0900

    Fix exports style

--- back/src/passport/strategy/naver.js
+++ back/src/passport/strategy/naver.js
@@ -1,12 +1,11 @@
 const { User } = require("../../db");
-
 const passport = require("passport");
 const {
   Strategy: NaverStrategy,
   Profile: NaverProfile,
 } = require("passport-naver-v2");
 
-module.exports = () => {
+const naver = () => {
   passport.use(
     new NaverStrategy(
       {
@@ -15,25 +14,21 @@ module.exports = () => {
         callbackURL: "/users/naver/callback",
       },
       async (accessToken, refreshToken, profile, done) => {
-        console.log("naver profile : ", profile);
         try {
           const exUser = await User.findById({
             // 네이버 플랫폼에서 로그인 했고 & snsId필드에 네이버 아이디가 일치할경우
-            where: { id: profile.id },
+            id: profile.id,
           });
           // 이미 가입된 네이버 프로필이면 성공
           if (exUser) {
+            console.log("success");
             done(null, exUser);
           } else {
-            // 가입되지 않는 유저면 회원가입 시키고 로그인을 시킨다
-            const userInfo = {
+            // 가입되지 않은 유저면 회원가입 시키고 로그인을 시킨다
+            const newUser = await User.create({
               id: profile.id,
-              email: profile.email,
-              name: profile.name,
-              birthday: profile.birthday,
               provider: "naver",
-            };
-            const newUser = await User.create({ userInfo });
+            });
             done(null, newUser);
           }
         } catch (error) {
@@ -44,3 +39,5 @@ module.exports = () => {
     )
   );
 };
+
+module.exports = { naver };
