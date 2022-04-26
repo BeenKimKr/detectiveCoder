7a946a65c9795a12f3f2c637caf6bb3ae706ef03
[4기-1반] 김경빈 <bin9610@gmail.com>
2022-04-26 16:38:32 +0900

    Add Api info for swagger docs

--- back/src/db/schemas/user.js
+++ back/src/db/schemas/user.js
@@ -1,5 +1,29 @@
 const { Schema, model } = require("mongoose");
 
+/**
+ * @swagger
+ *  components:
+ *    schemas:
+ *      User:
+ *        type: object
+ *        required:
+ *          - id
+ *          - provider
+ *        properties:
+ *          id:
+ *            type: String
+ *          provider:
+ *            type: String
+ *            description: Source of data
+ *          badge:
+ *            type: Array
+ *            description: Badges received by the user
+ *        example:
+ *           id: 6Ffds790H-hiOB8fdGd70F7sLg_dfFD90FI82
+ *           provider: 'naver'
+ *           badge: ['Germany', 'the United States']
+ */
+
 const UserSchema = new Schema(
     {
         id: {
--- back/src/routers/userRouter.js
+++ back/src/routers/userRouter.js
@@ -2,10 +2,39 @@ const { Router } = require("express");
 const passport = require("passport");
 const { userAuthService } = require("../services/userService");
 
+/**
+ * @swagger
+ * tags:
+ *   name: Users
+ *   description: API to manage users
+ */
 const userAuthRouter = Router();
 
+/**
+ * @swagger
+ * path:
+ *  /naver:
+ *    get:
+ *      summary: Register using Naver
+ *      tags: [Users]
+ */
 userAuthRouter.get("/naver", passport.authenticate("naver"));
 
+/**
+ * @swagger
+ * path:
+ *  /naver/callback:
+ *    get:
+ *      summary: Get user
+ *      tags: [Users]
+ *      responses:
+ *        "200":
+ *          description: Callback for users logged in to Naver
+ *          content:
+ *            application/json:
+ *              schema:
+ *                $ref: '#/components/schemas/User'
+ */
 userAuthRouter.get(
     "/naver/callback",
     passport.authenticate("naver", {
@@ -16,8 +45,31 @@ userAuthRouter.get(
     }
 );
 
+/**
+ * @swagger
+ * path:
+ *  /kakao:
+ *    get:
+ *      summary: Register using Kakao
+ *      tags: [Users]
+ */
 userAuthRouter.get("/kakao", passport.authenticate("kakao"));
 
+/**
+ * @swagger
+ * path:
+ *  /kakao/callback:
+ *    get:
+ *      summary: Get user
+ *      tags: [Users]
+ *      responses:
+ *        "200":
+ *          description: Callback for users logged in to Kakao
+ *          content:
+ *            application/json:
+ *              schema:
+ *                $ref: '#/components/schemas/User'
+ */
 userAuthRouter.get(
     "/kakao/callback",
     passport.authenticate("kakao", {
