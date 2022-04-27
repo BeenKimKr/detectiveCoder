e13d375ceb2e9bcc5c4bc22ae6ed6878dd3b72cf
[4기-1반] 양권민 <didrnjsals@gmail.com>
2022-04-27 17:58:12 +0900

    Fix space

--- back/src/swagger.js
+++ back/src/swagger.js
@@ -1,26 +1,26 @@
-const swaggerUi = require('swagger-ui-express');
-const swaggerJsdoc = require('swagger-jsdoc');
+const swaggerUi = require("swagger-ui-express");
+const swaggerJsdoc = require("swagger-jsdoc");
 
 const options = {
-    swaggerDefinition: {
-        openapi: '3.0.0',
-        info: {
-            title: 'CountryGoGo API',
-            version: '1.0.0',
-            description: 'CountryGoGo API with express'
-        },
-        servers: [
-            {
-                url: `http://localhost:${process.env.SERVER_PORT}`
-            },
-        ],
+  swaggerDefinition: {
+    openapi: "3.0.0",
+    info: {
+      title: "CountryGoGo API",
+      version: "1.0.0",
+      description: "CountryGoGo API with express",
     },
-    apis: ['./src/db/schemas/*.js', './src/routers/*.js']
+    servers: [
+      {
+        url: `http://localhost:${process.env.SERVER_PORT}`,
+      },
+    ],
+  },
+  apis: ["./src/db/schemas/*.js", "./src/routers/*.js"],
 };
 
 const specs = swaggerJsdoc(options);
 
 module.exports = {
-    swaggerUi,
-    specs
+  swaggerUi,
+  specs,
 };
