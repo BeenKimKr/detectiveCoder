9b2fb03cefebf4ebe8d11e0c1e2fa9424f3880f9
[4기-1반] 양권민 <didrnjsals@gmail.com>
2022-04-27 17:57:32 +0900

    Add countryRouter

--- back/src/app.js
+++ back/src/app.js
@@ -1,8 +1,9 @@
 const express = require("express");
-const { swaggerUi, specs } = require('./swagger');
+const { swaggerUi, specs } = require("./swagger");
 const passport = require("passport");
 const passportConfig = require("./passport");
 const { userAuthRouter } = require("./routers/userRouter");
+const { countryRouter } = require("./routers/countryRouter");
 const { errorMiddleware } = require("./middlewares/errorMiddleware");
 
 const app = express();
@@ -10,19 +11,20 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use((req, res, next) => {
-    req.accepts("application/json");
-    next();
+  req.accepts("application/json");
+  next();
 });
-app.use('/swagger', swaggerUi.serve, swaggerUi.setup(specs));
+app.use("/swagger", swaggerUi.serve, swaggerUi.setup(specs));
 
 // 기본 페이지
 app.get("/", (req, res) => {
-    res.send("main page");
+  res.send("main page");
 });
 
 passportConfig();
 app.use(passport.initialize());
 app.use("/users", userAuthRouter);
+app.use("/survey", countryRouter);
 app.use(errorMiddleware);
 
 module.exports = { app };
