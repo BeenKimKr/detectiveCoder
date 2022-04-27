6efff7774082c34dc5e0dff57e64dcf326833985
Naayeon <naaay097@gmail.com>
2022-04-28 06:44:08 +0900

    Fix Q

--- front/src/components/survey/SurveyContainer.js
+++ front/src/components/survey/SurveyContainer.js
@@ -1,85 +1,108 @@
 import React, { useState, useContext, useEffect } from 'react';
-import './style.css';
-import { SurveyFirstQuestion } from './question/SurveyFirstQuestion';
-import { SurveySecondQuestion } from './question/SurveySecondQuestion';
+import { QUESTIONS } from './question/QUESTIONS';
+import { FirstQuestion } from './question/FirstQuestion';
 import { SaveAnswersContext } from '../../pages/MainSurvey';
 import { PercentContext } from '../../pages/MainSurvey';
+import Button from '../../components/Button/CommonButton';
+
+import './style.css';
 
 const SurveyContainer = () => {
   const [index, setIndex] = useState(0);
   const { answer, answerDispatch } = useContext(SaveAnswersContext);
-  const { step, setStep, percent, setPercent } = useContext(PercentContext);
-  const [display, setDisplay] = useState([]);
+  const { percent, setPercent } = useContext(PercentContext);
+  const [tempArray, setTempArray] = useState([]);
+  const [finalArray, setFinalArray] = useState([]);
+  const [nextStep, setNextStep] = useState(false);
+  const [step, setStep] = useState(1);
 
-  const clickOption = (e) => {
-    if (SurveyFirstQuestion.length == index + 1) {
+  const handleClickAnswer = (e) => {
+    if (step == 1) {
       answerDispatch({ type: 'INPUT', data: e.currentTarget.value });
-      setPercent(percent + 20);
-      setIndex(0);
-      setStep(2);
-      console.log(index);
-    } else {
       setIndex(index + 1);
       setPercent(percent + 20);
+      if (FirstQuestion.length == index + 1) {
+        setPercent(percent + 20);
+        setIndex(0);
+        setTempArray(answer.slice(1, 5));
+        setStep(2);
+      }
+      //  두번째
+    } else if (step == 2) {
+      setNextStep(false);
+      console.log('step2', tempArray);
+      setFinalArray([...finalArray, e.currentTarget.value]);
       answerDispatch({ type: 'INPUT', data: e.currentTarget.value });
+      setIndex(index + 1);
+      if (tempArray.length == index + 2) {
+        setNextStep(true);
+        setTempArray(answer.slice(5, 7));
+        setNextStep(true);
+        setIndex(0);
+      }
     }
-  }; // index == 4
+    //  세번째
+    else {
+      console.log('step3', tempArray);
+    }
+  };
 
   useEffect(() => {
-    if (step == 2) {
-      console.log(answer);
-      console.log(display);
-      const temp = answer.slice(1, 6);
-      setDisplay([temp[0], temp[1]].join(''));
-      setDisplay([temp[2], temp[3]].join(''));
-    }
-  }, [step, answer, display]);
-  // 로직
+    setTempArray(answer.slice(5, 7));
+  }, [nextStep, answer]);
+
   return (
-    <div className="AnswerContainer">
-      {step === 1 ? (
-        <>
-          <button
-            className="AnswerCard"
-            onClick={clickOption}
-            value={SurveyFirstQuestion[index].options[0].id}
-          >
-            <h5>{SurveyFirstQuestion[index].options[0].option}</h5>
-          </button>
-          <button
-            className="AnswerCard"
-            onClick={clickOption}
-            value={SurveyFirstQuestion[index].options[1].id}
-          >
-            <h5>{SurveyFirstQuestion[index].options[1].option}</h5>
-          </button>
-        </>
-      ) : (
-        <div>
-          {SurveySecondQuestion.filter((it) => it.id == display[index]).map(
-            (x) => (
-              <>
-                <p>{x.Q}</p>
-                <button
-                  className="AnswerCard"
-                  onClick={clickOption}
-                  value={x.options[0].option}
-                >
-                  <h5>{x.options[0].option}</h5>
-                </button>
-                <button
-                  className="AnswerCard"
-                  onClick={clickOption}
-                  value={x.options[1].option}
-                >
-                  <h5>{x.options[1].option}</h5>
-                </button>
-              </>
-            )
+    <>
+      <div>
+        <div className="AnswerContainer">
+          {step === 1 ? (
+            <>
+              <button
+                className="AnswerCard"
+                onClick={handleClickAnswer}
+                value={FirstQuestion[index].options[0].id}
+              >
+                <h5>{FirstQuestion[index].options[0].option}</h5>
+              </button>
+              <button
+                className="AnswerCard"
+                onClick={handleClickAnswer}
+                value={FirstQuestion[index].options[1].id}
+              >
+                <h5>{FirstQuestion[index].options[1].option}</h5>
+              </button>
+            </>
+          ) : (
+            <>
+              {QUESTIONS.filter((it) => it.id == tempArray[index]).map((x) => (
+                <>
+                  <button
+                    className="AnswerCard"
+                    value={[tempArray[index]]}
+                    onClick={handleClickAnswer}
+                  >
+                    <h5>{x.descriptions[tempArray[index + 1]]}</h5>
+                  </button>
+                </>
+              ))}
+              {QUESTIONS.filter((it) => it.id == tempArray[index + 1]).map(
+                (x) => (
+                  <>
+                    <button
+                      className="AnswerCard"
+                      value={[tempArray[index + 1]]}
+                      onClick={handleClickAnswer}
+                    >
+                      <h5>{x.descriptions[tempArray[index]]}</h5>
+                    </button>
+                  </>
+                )
+              )}
+            </>
           )}
         </div>
-      )}
-    </div>
+      </div>
+    </>
   );
 };
 
--- front/src/components/survey/question/FirstQuestion.js
+++ front/src/components/survey/question/FirstQuestion.js
@@ -1,13 +1,13 @@
-export const SurveyFirstQuestion = [
+export const FirstQuestion = [
   {
     Q: '사회 복지 vs 청렴도',
     options: [
       {
-        id: '사회복지',
+        id: 'socialSupport',
         option: '나라는 부패했지만, 사회복지는 좋은 나라',
       },
       {
-        id: '청렴도',
+        id: 'corruption',
         option: '청렴하지만, 사회복지가 없는 나라',
       },
     ],
@@ -16,11 +16,11 @@ export const SurveyFirstQuestion = [
     Q: '삶의 자유 vs 물가',
     options: [
       {
-        id: '삶의자유',
+        id: 'Freedom',
         option: '매일 풀만 먹는 남한에서 살기. 빅맥이 귀한 음식...🍔',
       },
       {
-        id: '물가',
+        id: 'price',
         option: '좋아하는 음식 마음껏 먹는 북한에서 살기.',
       },
     ],
@@ -29,11 +29,11 @@ export const SurveyFirstQuestion = [
     Q: '경제 vs 기온',
     options: [
       {
-        id: '경제',
+        id: 'GDP',
         option: '경제는 좋지만 선호하는 계절이 없는 나라에서 살기.',
       },
       {
-        id: '기온',
+        id: 'temperature',
         option: '경제는 안 좋지만, 기온이 엄청 좋은 나라에서 살기',
       },
     ],
@@ -42,11 +42,11 @@ export const SurveyFirstQuestion = [
     Q: '기대 수명 vs 관대함',
     options: [
       {
-        id: '기대수명',
+        id: 'HLE',
         option: '100세까지 살지만, 평생 이웃 친구 하나없이 살기.',
       },
       {
-        id: '관대함',
+        id: 'Generosity',
         option: '10살까지 살지만,  국민들 성격 모두 좋은 나라에서 살기',
       },
     ],
