3b0014f9488e8104cff6ba726f0bec32dbb8437e
[4기-1반] 김경빈 <bin9610@gmail.com>
2022-04-27 18:10:34 +0900

    Refactoring before merging

--- back/src/passport/index.js
+++ back/src/passport/index.js
@@ -4,20 +4,20 @@ const { kakao } = require("./strategy/kakao");
 const { naver } = require("./strategy/naver");
 
 module.exports = () => {
-    passport.serializeUser((user, done) => {
-        done(null, user.id);
-    });
+  passport.serializeUser((user, done) => {
+    done(null, user.id);
+  });
 
-    passport.deserializeUser(async (id, done) => {
-        try {
-            const user = await User.findOne({ where: { id } });
-            done(null, user);
-        } catch (error) {
-            console.error(error);
-            done(error);
-        }
-    });
+  passport.deserializeUser(async (id, done) => {
+    try {
+      const user = await User.findOne({ where: { id } });
+      done(null, user);
+    } catch (error) {
+      console.error(error);
+      done(error);
+    }
+  });
 
-    naver();
-    kakao();
+  naver();
+  kakao();
 };
