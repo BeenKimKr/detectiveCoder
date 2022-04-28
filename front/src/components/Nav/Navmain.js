58ca1c1aad850285c550ee05139d719c05f57142
Hailey_ys <whassup_man@naver.com>
2022-04-28 15:51:56 +0900

    Fix nav

--- front/src/components/Nav/Navmain.js
+++ front/src/components/Nav/Navmain.js
@@ -4,41 +4,36 @@ import './Nav.css';
 
 const Navmain = () => {
   return (
-    <nav class="nav container bg-transparent px-2 sm:px-4 py-2.5 rounded">
-      <div class="container flex flex-wrap justify-between items-center mx-auto">
-        <a href="#" class="flex items-center">
+    <nav class='nav' id='nav'>
+      <div id='img'>
+        <a href='#'>
           <img
             src={process.env.PUBLIC_URL + '/imgs/CountryGOGO.png'}
-            class="mr-3 w-32 "
-            alt="logo"
+            class='mr-3 w-32 '
+            alt='logo'
           />
         </a>
 
-        <div class="hidden w-full md:block md:w-auto" id="mobile-menu">
-          <ul class="flex flex-col mt-4 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium">
+        <div class='hidden w-full md:block md:w-auto' id='mobile-menu'>
+          <ul>
             <li>
-              <a
-                href="#"
-                class="font-black text-base block py-2 pr-4 pl-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 "
-                aria-current="page"
-              >
+              <a href='#' aria-current='page'>
                 Home
               </a>
             </li>
+
             <li>
               <a
-                href="#"
-                class="font-extrabold text-base block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0  md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
-              >
+                href='#'
+                class='font-extrabold text-base block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0  md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700'>
                 다른 도시 보기
               </a>
             </li>
 
             <li>
               <a
-                href="#"
-                class="font-extrabold text-base block rounded-full py-2 pr-4 pl-3 text-gray-700 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-              >
+                href='#'
+                class='font-extrabold text-base block rounded-full py-2 pr-4 pl-3 text-gray-700 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent'>
                 Log Out
               </a>
             </li>
