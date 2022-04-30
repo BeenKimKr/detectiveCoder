d9408c7f9e4d6be97faa9b7ad51f3650d77788e0
Naayeon <naaay097@gmail.com>
2022-04-30 22:41:44 +0900

    Add button

---
+++ front/src/components/button/CommonButton.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './style.css';
+
+//button pull error
+const CommonButton = ({ text, type, onClick, disabled }) => {
+  const btnType = ['main', 'sub', 'temp'].includes(type) ? type : 'main';
+  return (
+    <button
+      className={['myBtn', `btn-${btnType}`].join(' ')}
+      onClick={onClick}
+      disabled={disabled}
+    >
+      {text}
+    </button>
+  );
+};
+
+export default CommonButton;
