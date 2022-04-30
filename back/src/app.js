a19655d4bf66a5f1cc62616915dfe08555f78974
[4기-1반] 김경빈 <bin9610@gmail.com>
2022-04-30 22:14:33 +0900

    Add cookie parser

--- back/src/app.js
+++ back/src/app.js
@@ -1,23 +1,27 @@
 const express = require("express");
 const { swaggerUi, specs } = require("./swagger");
 const cors = require("cors");
+const cookieParser = require("cookie-parser");
 const passport = require("passport");
 const passportConfig = require("./passport");
 const { userAuthRouter } = require("./routers/userRouter");
 const { countryRouter } = require("./routers/countryRouter");
 const { tempRouter } = require("./routers/tempRouter");
+const { cookieRouter } = require("./routers/cookieRouter");
 
 const { errorMiddleware } = require("./middlewares/errorMiddleware");
 
 const app = express();
 
-app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use((req, res, next) => {
   req.accepts("application/json");
   next();
 });
+app.use(cors());
+app.use(cookieParser());
+
 app.use("/swagger", swaggerUi.serve, swaggerUi.setup(specs));
 
 // 기본 페이지
@@ -30,6 +34,7 @@ app.use(passport.initialize());
 app.use("/users", userAuthRouter);
 app.use("/happy", countryRouter);
 app.use("/temp", tempRouter);
+app.use("/cookie", cookieRouter);
 app.use(errorMiddleware);
 
 module.exports = { app };
