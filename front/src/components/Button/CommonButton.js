b0798b4e22a94f0027448b5a9a3c8e773ec3ef72
Naayeon <naaay097@gmail.com>
2022-04-30 14:58:17 +0900

    Fix button error

--- front/src/components/Button/CommonButton.js
+++ front/src/components/Button/CommonButton.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './style.css';
 
+//button pull error
 const CommonButton = ({ text, type, onClick, disabled }) => {
   const btnType = ['main', 'sub', 'temp'].includes(type) ? type : 'main';
   return (
