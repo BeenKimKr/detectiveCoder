14d6213a2026d461865707ac6680e909690d7e29
Hailey_ys <whassup_man@naver.com>
2022-05-01 00:52:17 +0900

    Fix code

--- front/src/components/Nav/Navmain.js
+++ front/src/components/Nav/Navmain.js
@@ -5,7 +5,7 @@ const Navmain = () => {
   return (
     <nav class='bg-white border-gray-200 px-2 sm:px-4 py-2.5 rounded'>
       <div class='container flex flex-wrap justify-between items-center mx-auto'>
-        <a href='#' class=''>
+        <a href='/main' class=''>
           <img
             src={process.env.PUBLIC_URL + '/imgs/CountryGOGO.png'}
             class='mr-3 h-32 w-32 sm:h-9'
@@ -44,22 +44,22 @@ const Navmain = () => {
           <ul class='flex flex-col mt-4 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium'>
             <li>
               <a
-                href='#'
-                class='block py-2 pr-4 pl-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white'
+                href='/main'
+                class='block py-2 pr-4 pl-3 text-white bg-blue-700 rounded md:bg-transparent md:text-custom-main md:p-0 dark:text-white'
                 aria-current='page'>
                 Home
               </a>
             </li>
             <li>
               <a
-                href='#'
-                class='block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700'>
+                href='/AllCities'
+                class='block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-custom-main md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700'>
                 다른 도시 보기
               </a>
             </li>
 
             <li>
-              <a>로그아웃</a>
+              <a href='KakaoHome'>Kakao Login</a>
             </li>
           </ul>
         </div>
