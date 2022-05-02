612ff3058191a06e964a31a1961cd73762a90a3b
Hailey_ys <whassup_man@naver.com>
2022-05-02 22:31:42 +0900

    fail kakao

--- front/src/components/Kakao/OAuth.js
+++ front/src/components/Kakao/OAuth.js
@@ -3,3 +3,5 @@ const CLIENT_ID = '4db0ceddd72b85015bb53fc2b6c81918';
 const REDIRECT_URI = 'http://localhost:3000/users/kakao/callback';
 
 export const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=code`;
+
+// https://kauth.kakao.com/oauth/authorize?client_id=4db0ceddd72b85015bb53fc2b6c81918&redirect_uri=http://localhost:3000/users/kakao/callback&response_type=code
