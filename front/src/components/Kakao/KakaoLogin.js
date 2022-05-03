8bf455da5ceda63d44b8fdf7c1e8ed4d09e421f1
[4기-1반] 김경빈 <bin9610@gmail.com>
2022-05-03 21:02:47 +0900

    Add sns login button

--- front/src/components/Kakao/KakaoLogin.js
+++ front/src/components/Kakao/KakaoLogin.js
@@ -1,17 +1,31 @@
-import React, { Component } from 'react';
-import { KAKAO_AUTH_URL } from '../Kakao/OAuth';
-import { useNavigate } from 'react-router-dom';
-import axios from 'axios';
+import React, { useContext } from 'react';
+import { CLIENT_ID } from './OAuth';
+// import KaKaoLogin from 'react-kakao-login';
+import * as Api from '../../api';
 
-const KakaoLogin = () => {
-  console.log('KakaoLogin');
+const Login = () => {
+  const onClick = async () => {
+    console.log('onClick');
+    try {
+      // "user/login" 엔드포인트로 post요청함.
+      const res = await Api.get('users/current');
+      const currentUser = res.data;
+      console.log(res);
+      // console.log(currentUser);
+
+    } catch (err) {
+      // if (err.response) {
+      //   console.log(error);
+      // }
+      console.log('실패');
+    }
+  };
   return (
-    <>
-      <a href="http://localhost:5001/users/kakao">
-        <img src="//k.kakaocdn.net/14/dn/btqCn0WEmI3/nijroPfbpCa4at5EIsjyf0/o.jpg" />{' '}
-      </a>
-    </>
+    <a href='http://localhost:5001/users/kakao' onClick={onClick}>
+      {' '}
+      <img src='//k.kakaocdn.net/14/dn/btqCn0WEmI3/nijroPfbpCa4at5EIsjyf0/o.jpg' />
+    </a>
   );
 };
 
-export default KakaoLogin;
+export default Login;
