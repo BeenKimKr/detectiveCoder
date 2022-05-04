879809a4fcc366b07060499b6b9797be84e1474a
[4기-1반] 김경빈 <bin9610@gmail.com>
2022-05-05 03:23:33 +0900

    Fix userService for kakao login

--- back/src/services/userService.js
+++ back/src/services/userService.js
@@ -15,7 +15,15 @@ const userAuthService = {
     if (exUser) {
       return { token, userInfo: exUser };
     } else {
-      const userInfo = userProfile.kakao_account;
+      const userInfo = {
+        id: userId,
+        provider: 'kakao',
+        name: userProfile.kakao_account.profile.nickname,
+        email: userProfile.kakao_account.email,
+        gender: userProfile.kakao_account.gender,
+        age: userProfile.kakao_account.age_range,
+        birthday: userProfile.kakao_account.birthday
+      };
       const newUser = await User.create(userInfo);
       return { token, userInfo: newUser };
     }
