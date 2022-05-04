126242afe7b25972f3bf3380eec0462b20b1bdea
Hailey_ys <whassup_man@naver.com>
2022-05-05 00:00:40 +0900

    Fix redirect

--- front/src/components/Kakao/RedirectKakao.js
+++ front/src/components/Kakao/RedirectKakao.js
@@ -1,28 +1,30 @@
 import React, { useContext, useEffect } from 'react';
-import { DispatchContext } from '../../App';
+// import { saveResult } from '../../App';
 import { useNavigate } from 'react-router-dom';
 import * as Api from '../../api';
+import { ResultContext } from '../../App';
 
 const RedirectKakao = () => {
   const navigate = useNavigate();
-  const dispatch = useContext(DispatchContext);
+  // const dispatch = useContext(DispatchContext);
 
   let code = new URL(window.location.href).searchParams.get('code');
+  const { user, setUser } = useContext(ResultContext);
 
-  React.useEffect(() => {
-    dispatch(kakaoLogin(code));
-  }, []);
+  // // React.useEffect(() => {
+  // //   dispatch(kakaoLogin(code));
+  // }, []);
 
   const kakaoLogin = async (code) => {
     console.log('aa');
     try {
       const res = await Api.get(
-        `http://localhost:3000/users/kakao/callback?code=${code}`
+        `http://localhost:5001/users/kakao/callback?code=${code}`
       );
       console.log(res);
       // dispatch({
       //   type: 'LOGIN_SUCCESS',
-      //   // payload: user,
+      //   payload: user,
       // });
       navigate('/home');
     } catch (err) {
