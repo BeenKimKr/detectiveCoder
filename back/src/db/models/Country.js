a75326afd2823935beaac891a27ae2111d68105a
[4기-1반] 양권민 <didrnjsals@gmail.com>
2022-05-04 17:07:39 +0900

    Fix findBySurvey

--- back/src/db/models/Country.js
+++ back/src/db/models/Country.js
@@ -10,32 +10,29 @@ const Country = {
     return result;
   },
   findBySurvey: async (columns) => {
-    let temp = 0;
-    let first = "";
-    let second = "";
-    let third = "";
-    let third_ = "";
+    let temp = Number(columns[0]);
+    const first = columns[1];
+    const second = columns[2];
+    const third = columns[3];
+    const third_ = columns[4];
 
-    if (columns.length == 4) {
-      temp = Number(columns[0]);
-      first = columns[1];
-      second = columns[2];
-      third = columns[3];
+    if (columns.includes("temperature")) {
+      let dataFrame = await CountryModel.find({})
+        .where("mean")
+        .gt(temp - 4)
+        .lt(temp + 4)
+        .select(`Ab Country City ${first} ${second} ${third} ${third_}`);
+
+      return dataFrame;
     } else {
-      temp = Number(columns[0]);
-      first = columns[1];
-      second = columns[2];
-      third = columns[3];
-      third_ = columns[4];
+      const dataFrame = await CountryModel.find({})
+        .where("mean")
+        .gt(temp - 4)
+        .lt(temp + 4)
+        .select(`Ab Country City ${first} ${second} ${third} ${third_}`);
+
+      return dataFrame;
     }
-
-    const dataFrame = await CountryModel.find({})
-      .where("mean")
-      .gt(temp - 4)
-      .lt(temp + 4)
-      .select(`Ab Country City ${first} ${second} ${third} ${third_}`);
-
-    return dataFrame;
   },
 };
 
