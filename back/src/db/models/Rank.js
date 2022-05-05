faec4787ff4dab3b2ca8e3f8bd73682b64c4f786
[4기-1반] 양권민 <didrnjsals@gmail.com>
2022-05-05 16:35:07 +0900

    Fix error

--- back/src/db/models/Rank.js
+++ back/src/db/models/Rank.js
@@ -1,4 +1,4 @@
-const { RankModel } = require('../schemas/rank');
+const { RankModel } = require("../schemas/rank");
 
 const Rank = {
   findAll: async () => {
@@ -9,25 +9,9 @@ const Rank = {
     const result = await RankModel.findOne({ Country: Country });
     return result;
   },
-  findByColumn: async (column, offset) => {
-    if (Number(offset) === 12) {
-      const result = await RankModel.find({})
-        .select(`Ab Country ${column}`)
-        .limit(12);
-      return result;
-    } else if (Number(offset) === 69) {
-      const result = await RankModel.find({})
-        .skip(Number(offset))
-        .select(`Ab Country ${column}`)
-        .limit(2);
-      return result;
-    } else {
-      const result = await RankModel.find({})
-        .skip(Number(offset))
-        .select(`Ab Country ${column}`)
-        .limit(3);
-      return result;
-    }
+  findByColumn: async (column) => {
+    const result = await RankModel.find({}).select(`Ab Country ${column}`);
+    return result;
   },
 };
 
