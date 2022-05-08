2d79612d5c963378b5ee381a03af1ce890f24a17
[4기-1반] 김경빈 <bin9610@gmail.com>
2022-05-08 09:37:11 +0900

    Fixx

--- back/src/routers/commentRouter.js
+++ back/src/routers/commentRouter.js
@@ -1,18 +1,26 @@
-const { Router } = require("express");
-const { commentService } = require("../services/commentService");
+const { Router } = require('express');
+const { commentService } = require('../services/commentService');
 
 const commentRouter = Router();
 
 commentRouter.post('/', async (req, res, next) => {
-  const { score, comment } = req.body;
-  const createStatus = await commentService.addComment({ score, comment });
+  try {
+    const { score, comment } = req.body;
+    const createStatus = await commentService.addComment({ score, comment });
 
-  res.status(201).json(createStatus);
+    res.status(201).json(createStatus);
+  } catch (error) {
+    next(error);
+  }
 });
 
 commentRouter.get('/', async (req, res, next) => {
-  const comments = await commentService.getComments();
-  res.status(200).json(comments);
+  try {
+    const comments = await commentService.getComments();
+    res.status(200).send(comments);
+  } catch (error) {
+    next(error);
+  }
 });
 
 module.exports = { commentRouter };
